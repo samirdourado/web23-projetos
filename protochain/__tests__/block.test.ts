02d54f8bb2dfe17dbd98d5a09b75e4e0c44aeb51
samirdourado <samir3500@gmail.com>
2026-03-25 13:24:01 -0300

    Validação de classes

--- protochain/__tests__/block.test.ts
+++ protochain/__tests__/block.test.ts
@@ -12,13 +12,13 @@ describe("Block tests", () => {
     test('Should be valid', () => {
         const block = new Block(1, genesis.hash, "block 2");
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeTruthy();
+        expect(valid.success).toBeTruthy();
     })
 
     test('Should not be valid (previous hash)', () => {
         const block = new Block(1, "abc", "block 2");
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeFalsy();
+        expect(valid.success).toBeFalsy();
     })
 
     test('Should not be valid (timestamp)', () => {
@@ -26,25 +26,25 @@ describe("Block tests", () => {
         block.timestamp = -1;
         block.hash = block.getHash()
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeFalsy();
+        expect(valid.success).toBeFalsy();
     })
 
     test('Should not be valid (hash)', () => {
         const block = new Block(1, genesis.hash, "block 2");
         block.hash = "";
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeFalsy();
+        expect(valid.success).toBeFalsy();
     })
 
     test('Should not be valid (data)', () => {
         const block = new Block(1, genesis.hash, "");
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeFalsy();
+        expect(valid.success).toBeFalsy();
     })
 
     test('Should not be valid (index)', () => {
         const block = new Block(-1, genesis.hash, "block 2");
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeFalsy();
+        expect(valid.success).toBeFalsy();
     })
 })
