962dd7e4ea80a93d544305ccaeef0b93d0a08296
samirdourado <samir3500@gmail.com>
2026-04-02 22:40:19 -0300

    Feat: miner config start

---
+++ protochain/src/client/minerClient.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const BLOCKCHAIN_SERVER = 'http://localhost:3000/';
+
+async function mine() {
+    const { data } = await axios.get(`${BLOCKCHAIN_SERVER}blocks/next`)
+    console.log(data);
+}
+mine();
