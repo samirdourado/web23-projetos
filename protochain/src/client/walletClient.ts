75b177f86eaa1a001ae8cd2d73d350df79a5f854
samirdourado <samir3500@gmail.com>
2026-04-23 01:05:37 -0300

    Feat: Client generating and retrivieng wallet

--- protochain/src/client/walletClient.ts
+++ protochain/src/client/walletClient.ts
@@ -1,4 +1,102 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import axios from 'axios';
+import readline from 'readline';
 import Wallet from '../lib/wallet';
 
-const wallet = new Wallet('6a4ec778971bf0759b7e5d5f0629bb481ca8bfd008acc1ecfe826ef73c905ce2');
-console.log(wallet);
+const BLOCKCHAIN_SERVER = process.env.BLOCKCHAIN_SERVER;
+
+let myWalletPub = "";
+let myWalletPriv = "";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function menu() {
+    setTimeout(() => {
+        console.clear();
+
+        if (myWalletPub)
+            console.log(`You are logged as ${myWalletPub}.`);
+        else
+            console.log(`You aren't logged.`);
+
+        console.log("1 - Create Wallet");
+        console.log("2 - Recover Wallet");
+        console.log("3 - Balance");
+        console.log("4 - Send tx");
+        rl.question("Choose your option: ", (answer) => {
+            switch(answer) {
+                case "1": createWallet();break;
+                case "2": recoverWallet();break;
+                case "3": getBalance();break;
+                case "4": sendTx();break;
+                default: {
+                    console.log('Wrong option!');
+                    menu();
+                }
+            }
+        })
+
+
+    }, 1000)
+}
+
+function preMenu() {
+    rl.question(`Press enter to continue...`,() => {
+        menu();
+    })
+}
+
+function createWallet() {
+    console.clear();
+    const wallet = new Wallet();
+    console.log(`Your new wallet:`);
+    console.log(wallet);
+
+    myWalletPub = wallet.publicKey;
+    myWalletPriv = wallet.privateKey;
+    preMenu();
+}
+
+function recoverWallet() {
+    console.clear()
+    rl.question(`What is your private key or WIF ? `, (wifOrPrivateKey) => {
+        const wallet = new Wallet(wifOrPrivateKey);
+        console.log(`Your recovered wallet:`);
+        console.log(wallet);
+
+        myWalletPub = wallet.publicKey;
+        myWalletPriv = wallet.privateKey;
+        preMenu();
+    })
+}
+
+function getBalance() {
+    console.clear();
+
+    if (!myWalletPub) {
+        console.log(`You don't have a wallet yet.`);
+        return preMenu();
+    }
+
+    // TODO: get balance via API
+    preMenu();
+}
+
+function sendTx() {
+    console.clear();
+
+    if (!myWalletPub) {
+        console.log(`You don't have a wallet yet.`);
+        return preMenu();
+    }
+
+    // TODO: send tx via API
+    preMenu();
+}
+
+menu();
