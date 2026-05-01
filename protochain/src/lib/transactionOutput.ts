6ce7bf7f6d763d3bc0d2b0b9f82d5ab53cce62a0
samirdourado <samir3500@gmail.com>
2026-05-01 01:03:59 -0300

    feat: transactionOutput class

---
+++ protochain/src/lib/transactionOutput.ts
@@ -0,0 +1,28 @@
+import Validation from "./validation";
+import { SHA256 } from "crypto-js";
+
+/**
+ * Transaction Output class
+ */
+export default class TransactionOutput {
+    toAddress: string;
+    amount: number;
+    tx?: string;
+
+    constructor(txOutput?: TransactionOutput) {
+        this.toAddress = txOutput?.toAddress || "";
+        this.amount = txOutput?.amount || 0;
+        this.tx = txOutput?.tx || "";
+    }
+
+    isValid(): Validation {
+        if (this.amount < 1)
+            return new Validation(false, 'Negative amount');
+
+        return new Validation();
+    }
+
+    getHash(): string {
+        return SHA256(this.toAddress + this.amount + this.tx).toString();
+    }
+}
